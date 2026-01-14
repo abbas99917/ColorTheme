c3e7d3227c6c727ef032618b4784de171efdf335
abbas99917 <abbasgraphic17@gmail.com>
2026-01-14 00:09:31 -0800

    color them

---
+++ them.js
@@ -0,0 +1,61 @@
+function light(){
+    document.body.style.backgroundColor = "white";
+
+}
+
+
+function dark(){
+    document.body.style.backgroundColor = "black";
+    
+}
+
+function green(){
+    document.body.style.backgroundColor = "green";
+}
+
+function gold(){
+        document.body.style.backgroundColor = "gold";
+}
+function yellow(){
+    document.body.style.backgroundColor = "yellow";
+}
+
+function aqua(){
+    document.body.style.backgroundColor = "aqua";
+}
+function purple(){
+
+    document.body.style.backgroundColor = "purple";
+}
+function orange(){
+
+    document.body.style.backgroundColor = "orange";
+}
+function oliva(){
+
+    document.body.style.backgroundColor = "olive";
+}
+function indigo(){
+
+    document.body.style.backgroundColor = "indigo";
+}
+function magenta(){
+
+    document.body.style.backgroundColor = "magenta";
+}
+function blue(){
+
+    document.body.style.backgroundColor = "blue";
+}
+function pink(){
+
+    document.body.style.backgroundColor = "pink";
+}
+function brown(){
+
+    document.body.style.backgroundColor = "brown";
+}
+function red(){
+
+    document.body.style.backgroundColor = "red";
+}
