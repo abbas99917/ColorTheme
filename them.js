aa68eea50edc2f0ce829ec99e3f22969a36c107f
abbas99917 <abbasgraphic17@gmail.com>
2026-01-14 20:10:15 -0800

    add colour name

--- them.js
+++ them.js
@@ -1,61 +1,80 @@
-function light(){
-    document.body.style.backgroundColor = "white";
-
-}
-
-
-function dark(){
-    document.body.style.backgroundColor = "black";
+let title = document.getElementById("title") ; 
     
-}
+    
+    function light(){
+        document.body.style.backgroundColor = "white";
+         title.innerText = "Colour Theme: Light";
 
-function green(){
-    document.body.style.backgroundColor = "green";
-}
+    }
 
-function gold(){
-        document.body.style.backgroundColor = "gold";
-}
-function yellow(){
-    document.body.style.backgroundColor = "yellow";
-}
 
-function aqua(){
-    document.body.style.backgroundColor = "aqua";
-}
-function purple(){
+    function dark(){
+        document.body.style.backgroundColor = "black";
+        title.innerText = "Colour Theme: Black";
+        
+    }
 
-    document.body.style.backgroundColor = "purple";
-}
-function orange(){
+    function green(){
+        document.body.style.backgroundColor = "green";
+        title.innerText = "Colour Theme: Green";
+    }
 
-    document.body.style.backgroundColor = "orange";
-}
-function oliva(){
+    function gold(){
+            document.body.style.backgroundColor = "gold";
+            title.innerText = "Colour Theme: Gold";
+    }
+    function yellow(){
+        document.body.style.backgroundColor = "yellow";
+        title.innerText = "Colour Theme: Yellow";
+    }
 
-    document.body.style.backgroundColor = "olive";
-}
-function indigo(){
+    function aqua(){
+        document.body.style.backgroundColor = "aqua";
+        title.innerText = "Colour Theme: Aqua";
+    }
+    function purple(){
 
-    document.body.style.backgroundColor = "indigo";
-}
-function magenta(){
+        document.body.style.backgroundColor = "purple";
+        title.innerText = "Colour Theme: Purple";
+    }
+    function orange(){
 
-    document.body.style.backgroundColor = "magenta";
-}
-function blue(){
+        document.body.style.backgroundColor = "orange";
+        title.innerText = "Colour Theme: Orange";
+    }
+    function oliva(){
 
-    document.body.style.backgroundColor = "blue";
-}
-function pink(){
+        document.body.style.backgroundColor = "olive";
+        title.innerText = "Colour Theme: Olive";
+    }
+    function indigo(){
 
-    document.body.style.backgroundColor = "pink";
-}
-function brown(){
+        document.body.style.backgroundColor = "indigo";
+        title.innerText = "Colour Theme: Indigo";
+    }
+    function magenta(){
 
-    document.body.style.backgroundColor = "brown";
-}
-function red(){
+        document.body.style.backgroundColor = "magenta";
+        title.innerText = "Colour Theme: Magenta";
+    }
+    function blue(){
 
-    document.body.style.backgroundColor = "red";
-}
+        document.body.style.backgroundColor = "blue";
+        title.innerText = "Colour Theme: Blue";
+    }
+    function pink(){
+
+        document.body.style.backgroundColor = "Pink";
+        title.innerText = "Colour Theme: Pink";
+    }
+    function brown(){
+
+        document.body.style.backgroundColor = "Brown";
+        title.innerText = "Colour Theme: Brown";
+    }
+    function red(){
+
+        document.body.style.backgroundColor = "red";
+        title.innerText = "Colour Theme: Red";
+    }
+    
